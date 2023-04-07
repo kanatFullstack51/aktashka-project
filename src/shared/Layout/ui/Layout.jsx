dbe10c4389ef4c0594fb8c2b0f28fae393cafae8
kanatFullstack51 <kanatnazarov51@gmail.com>
2023-04-07 18:10:42 +0600

    added prettier

--- src/shared/Layout/ui/Layout.jsx
+++ src/shared/Layout/ui/Layout.jsx
@@ -2,10 +2,12 @@ import React, { Children } from 'react';
 import { Header } from 'src/widgets/Header';
 import { Footer } from 'src/widgets/Footer';
 
-export const Layout = ({children}) => {
-    return (
-     <><Header />
-     {children}
-     <Footer /></>
-    );
+export const Layout = ({ children }) => {
+  return (
+    <>
+      <Header />
+      {children}
+      <Footer />
+    </>
+  );
 };
